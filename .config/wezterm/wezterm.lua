5f8d24bfde0503a924c0ec6fe7c2c9a39c25733d
Hideo Hattori <hhatto.jp@gmail.com>
2025-01-06 14:08:14 +0900

    init wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.initial_rows = 50
+config.initial_cols = 250
+
+config.selection_word_boundary = ' \t\n{}[]()"\'`,;:'
+config.send_composed_key_when_left_alt_is_pressed = true
+-- config.send_composed_key_when_right_alt_is_pressed = false
+
+config.keys = {
+  {
+    key = "¥",
+    action = wezterm.action.SendKey { key = '\\' }
+  },
+}
+config.window_background_opacity = 0.89
+
+return config
